2f24470d46f664abb999d2735a50bdc5bbf0119e
github username <github email address>
2018-07-26 17:31:25 -0500

    Done.

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -1,1 +1,24 @@
+class Ingredient
+  @@all = []
+  
+  def initialize
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def self.most_common_allergen
+    blah = Hash.new(0)
+    Allergen.all.each do |allergen|
+      blah.key?(allergen.ingredient) ?
+      blah[allergen.ingredient] += 1 :
+      blah[allergen.ingredient] = 1 
+    end
+    arr = blah.to_a.sort_by do |i|
+      -i[1]
+    end
+    arr[0][0]
+  end
+end
